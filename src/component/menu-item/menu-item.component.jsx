8d0384c618c20bcde48730283ad4d2e87be9de21
Snehangshu Chakraborty <chakraborty.snehangshu@gmail.com>
2020-04-04 00:32:30 +0530

    first UI Commit

---
+++ src/component/menu-item/menu-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './menu-item.scss';
+const MenuItem = ({title, imageUrl})=>(
+    <div style = {{
+        backgroundImage : `url(${imageUrl})`
+    }} className="menu-item">
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+    </div>    
+);
+
+export default MenuItem;
