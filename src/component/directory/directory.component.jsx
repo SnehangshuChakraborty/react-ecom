ec935a0445240525fca0b9f54f4e62c0f9eb58b2
Snehangshu Chakraborty <chakraborty.snehangshu@gmail.com>
2020-04-05 04:04:05 +0530

    Modiefied Categories

--- src/component/directory/directory.component.jsx
+++ src/component/directory/directory.component.jsx
@@ -7,33 +7,33 @@ class Directory extends React.Component{
         super();
         this.state = {
             sections : [{
-                title: 'hats',
-                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                title: 'Fruits & Vegetables',
+                imageUrl: 'https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467__340.jpg',
                 id: 1,
-                linkUrl: 'shop/hats'
+                linkUrl: 'shop/fruits'
               },
               {
-                title: 'jackets',
-                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                title: 'groceries',
+                imageUrl: 'https://cdn.pixabay.com/photo/2017/08/09/18/56/shopping-2615482_960_720.jpg',
                 id: 2,
-                linkUrl: 'shop/jackets'
+                linkUrl: 'shop/groceries'
               },
               {
-                title: 'sneakers',
-                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                title: 'home & kitchen',
+                imageUrl: 'https://cdn.pixabay.com/photo/2017/09/16/00/33/spoon-2754133_960_720.jpg',
                 id: 3,
-                linkUrl: 'shop/sneakers'
+                linkUrl: 'shop/bouquets'
               },
               {
-                title: 'womens',
-                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                title: 'personal care',
+                imageUrl: 'https://cdn.pixabay.com/photo/2019/10/11/12/19/hair-care-4541744_960_720.jpg',
                 size: 'large',
                 id: 4,
-                linkUrl: 'shop/womens'
+                linkUrl: 'shop/personal'
               },
               {
-                title: 'mens',
-                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                title: 'household items',
+                imageUrl: 'https://cdn.pixabay.com/photo/2015/09/09/19/57/cleaning-932936_960_720.jpg',
                 size: 'large',
                 id: 5,
                 linkUrl: 'shop/mens'
